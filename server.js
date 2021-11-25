c1cf108f297e40d70fbe99f1e7154dce59213fac
LuuHiep1804 <luuhiep1804@gmail.com>
2021-11-25 13:36:54 +0700

    make it better

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import ward from './routers/ward.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
 
 const URI = process.env.DATABASE_URL;
 
