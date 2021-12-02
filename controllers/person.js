37394d12a344b06305d06455a46f5973d776a3c9
LuuHiep1804 <luuhiep1804@gmail.com>
2021-12-02 20:38:13 +0700

    update person

--- controllers/person.js
+++ controllers/person.js
@@ -1,4 +1,7 @@
+import { DistrictModel } from "../models/DistrictModel.js";
 import { PersonModel } from "../models/PersonModel.js";
+import { ProvinceModel } from "../models/ProvinceModel.js";
+import { RGroupModel } from "../models/RGroupModel.js";
 import { WardModel } from "../models/WardModel.js";
 
 export const getPerson = async (req, res) => {
@@ -6,27 +9,35 @@ export const getPerson = async (req, res) => {
         let allPerson 
         = await PersonModel.find()
         .populate({
-            path: 'ward_id',
+            path: 'residential_group_id',
             populate: {
-                path: 'district_id',
-                populate: {path: 'province_id'}
+                path: 'ward_id',
+                populate: {
+                    path: 'district_id',
+                    populate: {path: 'province_id'}
+                }
             }
         });
-        allPerson = allPerson.map((person) => {
+        allPerson = allPerson.map(person => {
+            const Rgroup = person.residential_group_id;
+            const ward = Rgroup.ward_id;
+            const district = ward.district_id;
+            const province = district.province_id;
             return {
                 _id: person._id,
                 name: person.name,
                 gender: person.gender,
                 date_of_birth: person.date_of_birth,
                 religion: person.religion,
-                ward: person.ward_id.name,
-                district: person.ward_id.district_id.name,
-                province: person.ward_id.district_id.province_id.name,
                 place_of_residence: person.place_of_residence,
                 academic_level: person.academic_level,
-                career: person.career
-            }
-        })
+                career: person.career,
+                residential_group: Rgroup.name,
+                ward: ward.name,
+                district: district.name,
+                province: province.name
+            };
+        });
         res.status(200).json(allPerson);
     } catch (err) {
         res.status(500).json({error: err});
@@ -38,26 +49,32 @@ export const getPersonById = async (req, res) => {
         const _id = req.params._id;
         const result = await PersonModel.findById(_id)
         .populate({
-            path: 'ward_id',
+            path: 'residential_group_id',
             populate: {
-                path: 'district_id',
+                path: 'ward_id',
                 populate: {
-                    path: 'province_id'
+                    path: 'district_id',
+                    populate: {path: 'province_id'}
                 }
             }
         });
+        const Rgroup = result.residential_group_id;
+        const ward = Rgroup.ward_id;
+        const district = ward.district_id;
+        const province = district.province_id;
         const person = {
             _id: result._id,
             name: result.name,
             gender: result.gender,
             date_of_birth: result.date_of_birth,
             religion: result.religion,
-            ward: result.ward_id.name,
-            district: result.ward_id.district_id.name,
-            province: result.ward_id.district_id.province_id.name,
             place_of_residence: result.place_of_residence,
             academic_level: result.academic_level,
-            career: result.career
+            career: result.career,
+            residential_group: Rgroup.name,
+            ward: ward.name,
+            district: district.name,
+            province: province.name
         };
         res.status(200).json(person);
     } catch (err) {
@@ -72,15 +89,13 @@ export const addPerson = async (req, res) => {
         if (checkPerson) {
             res.status(200).json('person is available!');
         }else {
-            const nameWard = req.body.ward;
-            const ward = await WardModel.findOne({name: nameWard});
             const newPerson = {
                 _id: infoPerson._id,
                 name: infoPerson.name,
                 gender: infoPerson.gender,
                 date_of_birth: infoPerson.date_of_birth,
                 religion: infoPerson.religion,
-                ward_id: ward._id,
+                residential_group_id: infoPerson.residential_group_id,
                 place_of_residence: infoPerson.place_of_residence,
                 academic_level: infoPerson.academic_level,
                 career: infoPerson.career
@@ -98,18 +113,6 @@ export const updatePerson = async (req, res) => {
     try {
         const _id = req.params._id;
         let infoUpdate = req.body;
-        if (infoUpdate.district) {
-            delete infoUpdate.district;
-        }
-        if (infoUpdate.province) {
-            delete infoUpdate.province;
-        }
-        if (infoUpdate.ward) {
-            const nameWard = infoUpdate.ward;
-            const ward = await WardModel.findOne({name: nameWard});
-            delete infoUpdate.ward;
-            infoUpdate.ward_id = ward._id;
-        }
         await PersonModel.findByIdAndUpdate(_id, infoUpdate);
         res.status(200).json('update success!');
     } catch (err) {
