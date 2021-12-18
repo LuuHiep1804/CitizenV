01151e9108062095bd2302953d028724d4af09e6
LuuHiep1804 <luuhiep1804@gmail.com>
2021-12-18 16:55:57 +0700

    update account

--- routers/analysis.js
+++ routers/analysis.js
@@ -1,7 +1,6 @@
 import express from "express";
-import { getAnalysisInGroup } from "../controllers/analysis.js";
+// import { getAnalysisInGroup } from "../controllers/analysis.js";
 
 const router = express.Router();
-
 
 export default router;
